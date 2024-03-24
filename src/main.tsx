d700ee48ed3867596a3782871a9022a226fae9f5
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-03-24 15:02:15 +0100

    feat: update method GA init

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/store.ts";
-import ReactGa from "react-ga";
 
 const store = setupStore();
-ReactGa.initialize(import.meta.env.VITE_GA_ID);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
