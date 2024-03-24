efdf81eb5b33254329f636911fb34da06a4b3e81
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-03-24 14:45:10 +0100

    feat: add google analytics initialize

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/store.ts";
+import ReactGa from "react-ga";
 
 const store = setupStore();
+ReactGa.initialize(import.meta.env.VITE_GA_ID);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
