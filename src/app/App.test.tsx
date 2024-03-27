166cb9912a83c9c8ac7fc5c244ec195633e7b3fe
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-03-27 18:41:49 +0100

    feat: add basic unit test for app.tsx

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,21 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import { App } from "./App.tsx";
-import { expect } from "vitest";
+import { describe } from "vitest";
 import { Provider } from "react-redux";
 import { setupStore } from "../store/store.ts";
 import { BrowserRouter } from "react-router-dom";
+import { ReactNode } from "react";
 
 const store = setupStore();
+vi.mock("../layouts", () => ({
+  MainLayout: ({ children }: { children: ReactNode }) => (
+    <div data-testid="main-layout">{children}</div>
+  ),
+}));
+vi.mock("../utils", () => ({
+  Public: () => <div data-testid="public-component"></div>,
+}));
+describe("App component", () => {
+  test("should load a App component", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>,
+    );
 
-it("should load a App component", () => {
-  render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>,
-  );
-  const message = screen.queryByText(/Alex Bielow/);
-  console.log(message?.innerHTML);
-  expect(message).toBeDefined();
+    const mainLayoutElement = screen.getByTestId("main-layout");
+    expect(mainLayoutElement).toBeInTheDocument();
+
+    const publicComponentElement = screen.getByTestId("public-component");
+    expect(publicComponentElement).toBeInTheDocument();
+  });
 });
